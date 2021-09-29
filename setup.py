ff893fdad2ef134ed53c684aaa9147bf3d73326a
TheKeineAhnung <fireball73@gmx.de>
2021-09-29 20:11:48 +0200

    🔖 new version tag (0.1.0)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='colorcodetools',
-    version='0.0.5',
+    version='0.1.0',
     author='Keine Ahnung',
     author_email='kontakt@keineahnung.eu',
     description='Some powerful color tools',
