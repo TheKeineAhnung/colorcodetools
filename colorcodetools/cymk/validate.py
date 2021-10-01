26b9918cd3c624c03783cfcd3bf0cdf0fac527d2
TheKeineAhnung <kontakt@keineahnung.eu>
2021-10-01 12:16:31 +0200

    :sparkles: added validate function for cymk codes (docs come later)

---
+++ colorcodetools/cymk/validate.py
@@ -0,0 +1,25 @@
+def is_cymk(cymk: str) -> bool:
+    try:
+        cymk_split = cymk.split("/")
+    except:
+        return False
+    for e in cymk_split:
+        try:
+            int(e)
+        except:
+            return False
+        if len(e) > 3:
+            return False
+        if int(e) > 100 or int(e) < 0:
+            return False
+    return True
+
+
+print(is_cymk("100/50/-1/54"))
+
+
+def is_single_cymk(cymk: int) -> bool:
+    if 100 >= cymk >= 0:
+        return True
+    else:
+        return False
