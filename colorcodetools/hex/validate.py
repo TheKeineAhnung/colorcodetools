1c2f1f6308ef7b0d89c3c58c2f1d59bf8c09fba8
TheKeineAhnung <fireball73@gmx.de>
2021-10-22 17:19:03 +0200

    :sparkles: added is_in_vga for checking if hexcode is in vga palette

--- colorcodetools/hex/validate.py
+++ colorcodetools/hex/validate.py
@@ -9,3 +9,20 @@ def is_hex(hexcode: str) -> bool:
         if e >= "g":
             return False
     return True
+
+
+def is_in_vga(hexcode: str) -> bool:
+    if hexcode.startswith('#'):
+        hex_check: str = hexcode.replace('#', '')
+    else:
+        hex_check: str = hexcode
+    if len(hex_check) != 6 and len(hex_check) != 3:
+        return False
+    if hex_check.lower() in __config_is_vga():
+        return True
+    return False
+
+
+def __config_is_vga() -> list:
+    return ['000000', '800000', '008000', '808000', '000080', '800080', '008080', 'c0c0c0', '808080', 'ff0000',
+            '00ff00', 'ffff00', '0000ff', 'ff00ff', '00ffff', 'ffffff']
