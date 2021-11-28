1e3635a4f23a5b71181ccc4050cd2fe77e3a43e7
TheKeineAhnung <fireball73@gmx.de>
2021-11-28 13:59:05 +0100

    🐛 fixed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='colorcodetools',
-    version='0.2.1',
+    version='0.3.0',
     author='Keine Ahnung',
     author_email='kontakt@keineahnung.eu',
     description='Some powerful color tools',
